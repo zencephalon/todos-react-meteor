53ff18e0a94b2098aff668dddbf3388a6f673dab
Matthew Bunday <zenceph@gmail.com>
2016-06-25 11:07:39 -0400

    Fix typo

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
-import { check } form 'meteor/check';
+import { check } from 'meteor/check';
  
 export const Tasks = new Mongo.Collection('tasks');
 
