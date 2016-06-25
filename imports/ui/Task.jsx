5e74cb594c8769db5af0662699041d89b59e6a86
Matthew Bunday <zenceph@gmail.com>
2016-06-25 11:24:13 -0400

    Lint roll Task.jsx

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,45 +1,43 @@
 import React, { Component, PropTypes } from 'react';
 import { Meteor } from 'meteor/meteor';
 
-import { Tasks } from '../api/tasks.js'
- 
 // Task component - represents a single todo item
 export default class Task extends Component {
-  toggleChecked() {
+  toggleChecked = () => (
     // Set the checked property to the opposite of its current value
-    Meteor.call('tasks.setChecked', this.props.task._id, !this.props.task.checked);
-  }
-  
-  deleteThisTask() {
-    Meteor.call('tasks.remove', this.props.task._id);
-  }
+    Meteor.call('tasks.setChecked', this.props.task._id, !this.props.task.checked)
+  )
+
+  deleteThisTask = () => (
+    Meteor.call('tasks.remove', this.props.task._id)
+  )
 
   render() {
-    const taskClassName = this.props.task.checked ? 'checked' : '';
+    const taskClassName = this.props.task.checked ? 'checked' : ''
 
     return (
       <li className={taskClassName}>
-        <button className="delete" onClick={this.deleteThisTask.bind(this)}>
+        <button className="delete" onClick={this.deleteThisTask}>
           &times;
         </button>
-      
+
         <input
           type="checkbox"
           readOnly
           checked={this.props.task.checked}
-          onClick={this.toggleChecked.bind(this)}
+          onClick={this.toggleChecked}
         />
 
         <span className="text">
           <strong>{this.props.task.username}</strong>: {this.props.task.text}
         </span>
       </li>
-    );
+    )
   }
 }
- 
+
 Task.propTypes = {
   // This component gets the task to display through a React prop.
   // We can use propTypes to indicate it is required
   task: PropTypes.object.isRequired,
-};
+}
